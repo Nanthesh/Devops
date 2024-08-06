e4cfd49ca677fded654cd96d553580bb5ad6a492
Nanthesh <nanthesh8kannan@gmail.com>
2024-08-05 23:29:32 -0400

    Add lint script and ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,11 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
-import pluginJest from "eslint-plugin-jest";
 
-export default {
-  files: ["**/*.{js,mjs,cjs,jsx}"],
-  languageOptions: {
-    ecmaVersion: 2021,
-    sourceType: "module",
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-    },
-    globals: {
-      test: "readonly",
-      expect: "readonly"
-    }
-  },
-  plugins: {
-    react: pluginReact,
-    jest: pluginJest,
-  },
-  rules: {
-    ...pluginReact.configs.recommended.rules, // Include recommended rules from react plugin
-    ...pluginJest.configs.recommended.rules, // Include recommended rules from jest plugin
-    "react/react-in-jsx-scope": "off",
-    "react/jsx-uses-react": "error",
-    "react/jsx-uses-vars": "error"
-  },
-  settings: {
-    react: {
-      version: "detect"
-    }
-  }
-};
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
