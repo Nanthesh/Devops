be2c467d166e8c269d745aeec105c7015f4f15cf
Nanthesh <nanthesh8kannan@gmail.com>
2024-08-06 04:45:21 -0400

    unorder list added

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ const App = () => {
   return (
     <>
     <h1>DEVOPS Project GROUP 5</h1>
-    <p>Git Hub</p>
-    <p>Git Hub pipeline</p>
-    <p>Junkins </p>
-    <p>Kubernet</p>
+    <ul>
+      <li>Git Hub</li>
+      <li>Git Hub pipeline</li>
+      <li>Docker</li>
+      <li>Junkins</li>
+      <li>Kubernet</li>
+    </ul>
     </>
   )
 }
