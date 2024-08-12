96ce91307207af4dcf1a5808bbc335f351d1f806
Nanthesh Kannan <nantheshkannan@Nantheshs-MacBook-Pro.local>
2024-08-12 17:57:55 -0400

    app.test.js edited

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders project title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/DEVOPS Project GROUP 5/i);
   expect(linkElement).toBeInTheDocument();
 });
+
